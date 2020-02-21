2e9ef11520d5673938c165da0b3a456946afc5f6
OperatorSDK Katacoda <operator-sdk@example.com>
2020-02-21 06:56:13 +0000

    Hellow

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>This is Second Time, My first PHP page</h1>
+<h1>This is Incridible but This is Second Time, My first PHP page</h1>
 
 <?php
 echo "Hello World, I Love my India! From Unnati Classes";
