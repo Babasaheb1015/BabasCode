8fd07b85581edcb5cbf327b52ae8281f4768851f
Babasaheb1015 <60650575+Babasaheb1015@users.noreply.github.com>
2020-03-05 12:36:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>This is Incridible but This is Second Time, My first PHP page</h1>
 
 <?php
-echo "Hello World, I Love my India! From Unnati Classes";
+echo "Hello World, This From Server1";
 ?>
 
 </body>
