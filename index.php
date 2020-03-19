40b87c1b686deccb42f2b500056da06c193d770b
OperatorSDK Katacoda <operator-sdk@example.com>
2020-03-19 13:52:24 +0000

    practiceno-1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1> This is Second Time, Its my first PHP page</h1>
 
 <?php
-echo " Shreya&Raj Hello World, This From Server 2 prepairing for examps";
+echo "PracticeNo.1  Shreya&Raj Hello World, This From Server 2 prepairing for examps";
 ?>
 
 </body>
